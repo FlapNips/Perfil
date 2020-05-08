9ef920c27482bf8f3be6166dee88864a8fc99190
Alfieri <63076580+br-alfieri@users.noreply.github.com>
2020-05-07 21:43:55 -0300

    Login Page Designe
    Primeiros passos

---
+++ personal/src/components/Login/Sign-In/CardsJoin.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="layout-cardsjoin">
+        teste
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<style scooped>
+#layout-cardsjoin {
+    margin-top: 10px;
+    height: 60px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 40%;
+    min-width: 400px;
+    background: gray;
+}
+</style>
---
+++ personal/src/components/Login/Sign-In/TextRight.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="layout-textright">
+        textright
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scooped>
+#layout-textright {
+    min-width: 200px;
+    width: 20%;
+    height: 10px;
+    background: chartreuse;
+}
+</style>
